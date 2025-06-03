9d52bf3c480d33d2f82e2aaa85c1e9aef50f4080
karthick <karthi22308@gmail.com>
2025-06-03 16:02:01 +0530

    e ignored, and an empty message aborts the commit.

---
+++ SDE/CPP_DSApractice-main/Essentials_Solution/Essentials/Essentials/Essentials.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+#define   WER "KARTHI"
+using namespace std;
+
+int main()
+{
+    cout << WER;
+    cout << "hello world"<<endl;
+    int m1;
+    int m2;
+    cin >> m1 >> m2;
+  
+    //typecasting
+    float avg = (m1 + m2) /(float) 2;
+    cout << "avg is " << avg;
+
+    return 0;
+}
+
+
