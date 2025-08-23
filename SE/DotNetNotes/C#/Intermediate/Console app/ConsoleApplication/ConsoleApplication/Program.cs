e320a00d6f935e3db395374a1af2c451012d81c2
karthick <karthi22308@gmail.com>
2025-08-23 16:22:36 +0530

    dotnet intermediate course commit

---
+++ SE/DotNetNotes/C#/Intermediate/Console app/ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace ConsoleApplication
+{
+    public class vehicle
+    {
+        private readonly string registrationnumber;
+
+        public vehicle(string registrationnumber)
+        {
+            this.registrationnumber = registrationnumber;
+        }
+
+    }
+    public class Car : vehicle 
+       
+    {
+
+        public Car(string registrationnumber )
+            :base( registrationnumber ) 
+        {
+            
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //section 2 
+            Program.classes();
+
+           
+
+
+        }
+        public static void  classes()
+        {
+            //params sample
+            Console.WriteLine(add(1, 2, 3, 4, 5, 6));
+            //out keyword sample
+
+            var input = "123";
+            int integ;
+            var result = int.TryParse(input, out integ);
+            if (result == false)
+            {
+                Console.WriteLine("conversion failed");
+            }
+            else
+            {
+                Console.WriteLine("the number is {0}", integ);
+            }
+            //indexers
+            var cookie = new HttpCookie();
+            cookie["name"] = "Mosh";
+            Console.WriteLine(cookie["name"]);
+        }
+        public static int add(params int[] ints)
+        {
+            var output = 0;
+            foreach (int i in ints)
+            {
+                output += i;
+            }
+            return output;
+        }
+    }
+}
--- Sync.bat
+++ Sync.bat
@@ -5,6 +5,9 @@ cd /d C:\MyNotes
 REM Add all changes
 git add .
 
+REM Ask for a commit message
+set /p user_comment=Enter commit message (leave empty to use date): 
+
 REM Get current date in dd/MM format and store in a variable
 for /f "tokens=1-3 delims=/ " %%a in ('date /t') do (
     set day=%%a
@@ -19,8 +22,12 @@ if %day% gtr 12 (
     set commit_date=%month%/%day%
 )
 
-REM Commit with message
-git commit -m "commit on %commit_date%"
+REM Check if user gave a comment
+if "%user_comment%"=="" (
+    git commit -m "commit on %commit_date%"
+) else (
+    git commit -m "%user_comment%"
+)
 
 REM Push to remote
 git push
