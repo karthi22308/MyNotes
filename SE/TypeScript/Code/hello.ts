74ee94626f2c0fc9965170cd8f38fc2313151950
Karthi ck <karthi22308@gmail.com>
2026-01-27 16:02:09 +0530

    TypeScript Commit

---
+++ SE/TypeScript/Code/hello.ts
@@ -0,0 +1,12 @@
+function greet(name?: string): string {
+  return `Hello, ${name || 'Stranger'}!`;
+}
+
+const message: any = greet();
+console.log(message);
+
+
+let  x : any = "karthick";
+x = 10;
+console.log(x);
+
